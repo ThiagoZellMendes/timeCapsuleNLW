4fe7916f3d10dec06a990329f6561bb8924004e8
Thiago Zell Mendes <thiagosmendes.th@gmail.com>
2023-05-19 19:27:10 -0300

    Refactor: Separate Code In Components(HomePege) - Web

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,6 +1,9 @@
-import { User } from "lucide-react";
-import nlwLogo from "../assets/logo.svg";
+import { SignIn } from "@/components/signIn";
 import Image from "next/image";
+import nlwLogo from "../assets/logo.svg";
+import { Copyright } from "@/components/copyright";
+import { Hero } from "@/components/hero";
+import { EmptyMemories } from "@/components/emptyMemories";
 
 export default function Home() {
   return (
@@ -13,77 +16,19 @@ export default function Home() {
         {/* Stripes */}
         <div className="absolute top-0 bottom-0 w-2 right-2 bg-stripes" />
 
-        {/* Sigh in */}
-        <a
-          href=""
-          className="flex items-center gap-3 text-left transition-colors hover:text-gray-50"
-        >
-          <div className="flex items-center justify-center w-10 h-10 bg-gray-400 rounded-full ">
-            <User className="w-5 h-5 text-gray-500 hover:text-gray-50" />
-          </div>
-
-          <p className="max-w-[140px] text-sm leading-snug">
-            <span className="underline">Crie sua conta</span> e salve suas
-            memórias!
-          </p>
-        </a>
+        {/* Sign in */}
+        <SignIn />
 
         {/* Hero */}
-        <div className="space-y-5">
-          <Image src={nlwLogo} alt="NLW Spacetime" />
-
-          <div className="max-w-[420px] space-y-1">
-            <h1 className="text-5xl font-bold leading-tight text-gray-50">
-              Sua cápsula do tempo
-            </h1>
-            <p className="leading-relaxed text-ls">
-              colecione momentos marcantes da sua joganda e compartilhe (se
-              quiser) com o mundo!
-            </p>
-          </div>
-
-          <a
-            className="inline-block px-5 py-3 text-sm leading-none text-black uppercase bg-green-500 rounded-full font-alt hover:bg-green-600"
-            href=""
-          >
-            CADASTRAR LEMBRANÇA
-          </a>
-        </div>
+        <Hero />
+        
         {/* Copyright */}
-        <div className="text-sm leading-relaxed text-gray-200">
-          Feito com 💜 por{" "}
-          <a
-            target="_blank"
-            rel="noreferrer"
-            className="underline hover:text-gray-100"
-            href="https://www.linkedin.com/in/thiago-mendes-44176249/"
-          >
-            Thiago Mendes
-          </a>
-          , no NLW da{" "}
-          <a
-            target="_blank"
-            rel="noreferrer"
-            className="underline hover:text-gray-100"
-            href="https://app.rocketseat.com.br/dashboard"
-          >
-            {" "}
-            Rocketseat
-          </a>
-        </div>
+        <Copyright />
       </div>
 
       {/* Right */}
       <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
-        <div className="flex items-center justify-center flex-1">
-          <p className="w-[360px] text-center leading-relaxed">
-            Você ainda não registrou nenhuma lembrança, comece a{" "}
-            <a href="" className="underline hover:text-gray-50">
-              criar agora
-            </a>
-            !
-          </p>
-        </div>
+        <EmptyMemories/>
       </div>
     </main>
   );
---
+++ web/src/components/hero/index.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import nlwLogo from "../../assets/logo.svg";
+
+export function Hero() {
+  return (
+    <div className="space-y-5">
+      <Image src={nlwLogo} alt="NLW Spacetime" />
+
+      <div className="max-w-[420px] space-y-1">
+        <h1 className="text-5xl font-bold leading-tight text-gray-50">
+          Sua cápsula do tempo
+        </h1>
+        <p className="leading-relaxed text-ls">
+          colecione momentos marcantes da sua joganda e compartilhe (se quiser)
+          com o mundo!
+        </p>
+      </div>
+
+      <a
+        className="inline-block px-5 py-3 text-sm leading-none text-black uppercase bg-green-500 rounded-full font-alt hover:bg-green-600"
+        href=""
+      >
+        CADASTRAR LEMBRANÇA
+      </a>
+    </div>
+  );
+}
