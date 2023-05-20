95489954e83ae7b59f0b02deba54204a68289082
Thiago Zell Mendes <thiagosmendes.th@gmail.com>
2023-05-19 21:13:07 -0300

    Feat: Implement Oauth with Github  And JWT - Server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,9 @@
+import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+import jwt from '@fastify/jwt'
 
 const app = fastify()
 
@@ -8,7 +11,12 @@ app.register(cors, {
   origin: true,
 })
 
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
 app.register(memoriesRoutes)
+app.register(authRoutes)
 
 app
   .listen({
