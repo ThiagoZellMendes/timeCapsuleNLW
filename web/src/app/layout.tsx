1d1ff3c154224ee1d1a9fa9eeec1e2248c57675a
Thiago Zell Mendes <thiagosmendes.th@gmail.com>
2023-05-24 22:13:23 -0300

    Refactor: Moved To LayoutFile All Defaults Screen - Web

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,31 +1,61 @@
-import { ReactNode } from 'react'
-import './globals.css'
+import { AuthProfile } from "@/components/authProfile";
+import { Copyright } from "@/components/copyright";
+import { EmptyMemories } from "@/components/emptyMemories";
+import { Hero } from "@/components/hero";
+import { SignIn } from "@/components/signIn";
 import {
-  Roboto_Flex as Roboto,
   Bai_Jamjuree as BaiJamjuree,
-} from 'next/font/google'
+  Roboto_Flex as Roboto,
+} from "next/font/google";
+import { cookies } from "next/headers";
+import { ReactNode } from "react";
+import "./globals.css";
 
-const roboto = Roboto({ subsets: ['latin'], variable: '--font-roboto' })
+const roboto = Roboto({ subsets: ["latin"], variable: "--font-roboto" });
 const baiJamjuree = BaiJamjuree({
-  subsets: ['latin'],
-  weight: '700',
-  variable: '--font-bai-jamjuree',
-})
+  subsets: ["latin"],
+  weight: "700",
+  variable: "--font-bai-jamjuree",
+});
 
 export const metadata = {
-  title: 'NLW Spacetime',
+  title: "NLW Spacetime",
   description:
-    'Uma cápsula do tempo construída com React, Next.js, TailwindCSS e TypeScript',
-}
+    "Uma cápsula do tempo construída com React, Next.js, TailwindCSS e TypeScript",
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
+  const isAuthenticated = cookies().has("token");
   return (
     <html lang="en">
       <body
         className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
       >
-        {children}
+        <main className="grid min-h-screen grid-cols-2">
+          {/* Left */}
+          <div className="relative flex flex-col items-start justify-between overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover px-28 py-16">
+            {/* Blur */}
+            <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
+
+            {/* Stripes */}
+            <div className="absolute top-0 bottom-0 w-2 right-2 bg-stripes" />
+
+            {/* Sign in */}
+            {isAuthenticated ? <AuthProfile /> : <SignIn />}
+
+            {/* Hero */}
+            <Hero />
+
+            {/* Copyright */}
+            <Copyright />
+          </div>
+
+          {/* Right */}
+          <div className="flex flex-col bg-[url(../assets/bg-stars.svg)] bg-cover p-16">
+            {children}
+          </div>
+        </main>
       </body>
     </html>
-  )
+  );
 }
